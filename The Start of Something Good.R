8b0670c3799f6fb3e82b8468ffb1faacd856dfbb
Jacob Johnson <JDJohnson1993@gmail.com>
2021-03-11 07:57:17 -0600

    Added a comment. This should be changes made to the "JJ first Branch" branch

--- The Start of Something Good.R
+++ The Start of Something Good.R
@@ -19,3 +19,8 @@ library(gt)
 ############################################################################################################################################################
 
 
+# Some how JJ First Brnach and main branch were swiched or... overridden
+
+
+# Now I am on JJ First Branch:
+# There is a button in the top right of R Studio that allows you to switch between branches
